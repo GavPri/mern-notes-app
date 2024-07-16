66688020176a231a752568adf2e6220de48c9d4e
德华 刘 <gavinprior5@gmail.com>
2024-07-16 18:31:29 +0000

    configure react-bootstrap
    remove top level node.module, package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
+import NavBar from "./components/Navigation/NavBar";
 
 function App() {
   return (
-    <div id="root" className="min-h-screen min-w-screen light bg-background">
-      {" "}
-      <p className="text-2xl text-copySecondary">Hello</p>
+    <div id="root" className="min-h-screen min-w-screen dark bg-background">
+      <NavBar />
     </div>
   );
 }
