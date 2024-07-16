b711f9cb14084de869b9f6de68d15df1bd0f0b88
德华 刘 <gavinprior5@gmail.com>
2024-07-16 18:55:10 +0000

    remove one link from navbar
    style navbar dropdown in index.css

--- client/src/components/Navigation/NavBar.js
+++ client/src/components/Navigation/NavBar.js
@@ -9,9 +9,11 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className="bg-cta">
       <Container fluid className="flex justify-between items-center">
-        <Navbar.Brand href="#" className="text-copyPrimary">Notes App</Navbar.Brand>
+        <Navbar.Brand href="#" className="text-copyPrimary">
+          Notes App
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -19,21 +21,37 @@ const NavBar = () => {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1" className="text-copyPrimary">Home</Nav.Link>
-            <Nav.Link href="#action2" className="text-copyPrimary">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3" className="text-copyPrimary">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4" className="text-copyPrimary">
+            <Nav.Link
+              href="#action1"
+              className="text-copyPrimary hover:text-copySecondary"
+            >
+              Home
+            </Nav.Link>
+            <NavDropdown
+              title="Theme"
+              id="navbarScrollingDropdown"
+              className="text-copyPrimary"
+            >
+              <NavDropdown.Item
+                href="#action3"
+                className="text-copyPrimary hover:text-copySecondary"
+              >
+                Action
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action4"
+                className="text-copyPrimary hover:text-copySecondary"
+              >
                 Another action
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
+              <NavDropdown.Item
+                href="#action5"
+                className="text-copyPrimary hover:text-copySecondary"
+              >
                 Something else here
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
